91a978dcbeb76cde92c4655563af04dfe3468206
fikri-mfa <muhammadfikria04@gmail.com>
2022-12-27 00:25:16 +0700

    i to d

--- dashboard.php
+++ dashboard.php
@@ -97,7 +97,7 @@ if (!isset($_SESSION['email'])) {
       <div id="main-menu" class="main-menu collapse navbar-collapse">
         <ul class="nav navbar-nav">
           <li class="active">
-            <a href="index.php"><i class="menu-icon fa ti-dashboard"></i>Dashboard</a>
+            <a href="dashboard.php"><i class="menu-icon fa ti-dashboard"></i>Dashboard</a>
           </li>
           <li class="menu-item-has-children dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
